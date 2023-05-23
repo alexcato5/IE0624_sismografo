1c8a4fd47247739e328aa1929cd5d55e58b7971b
alexcato <alexander.calderontorres@ucr.ac.cr>
2023-05-22 22:24:39 -0600

    Se agregó la biblioteca libopencm3 con el ejemplo base

---
+++ libopencm3-sismografo/examples/stm32/f2/jobygps/miniblink/miniblink.d
@@ -0,0 +1,15 @@
+miniblink.o: miniblink.c \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/f2/memorymap.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/f2/rcc.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/f2/gpio.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
---
+++ libopencm3-sismografo/examples/stm32/f2/jobygps/usart_printf/usart_printf.d
@@ -0,0 +1,37 @@
+usart_printf.o: usart_printf.c /usr/include/newlib/stdio.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/machine/ieeefp.h /usr/include/newlib/sys/features.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/sys/_pthreadtypes.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/sys/stdio.h \
+ /usr/include/newlib/errno.h /usr/include/newlib/sys/errno.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/f2/memorymap.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/f2/gpio.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/usart.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/f2/usart.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/usart_common_f24.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/usart_common_f124.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/usart_common_all.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/cm3/nvic.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/dispatch/nvic.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/f2/nvic.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/f2/rcc.h \
+ /home/alexcato/lab4/libopencm3-examples/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h
